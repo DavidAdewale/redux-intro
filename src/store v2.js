bc974673d6d803cdad966b157c11ce257614e69b
unknown <adewaledavidola@gmail.com>
2023-07-12 16:26:18 +0100

    Refactored the entire codebase using Modern Redux Toolkit

---
+++ src/store v2.js
@@ -0,0 +1,19 @@
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import accountReducer from './features/accounts/AccountSlice';
+import customerReducer from './features/customers/CustomerSlice';
+
+import { configureStore } from '@reduxjs/toolkit';
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;
