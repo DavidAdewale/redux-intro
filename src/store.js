2fdf886f74012ca6aef05ad3c183d7283f4ff147
unknown <adewaledavidola@gmail.com>
2023-07-12 10:23:00 +0100

    structured the project professionally

--- src/store.js
+++ src/store.js
@@ -1,57 +1,12 @@
-import { createStore } from 'redux';
+import { combineReducers, createStore } from 'redux';
+import accountReducer from './features/accounts/AccountSlice';
+import customerReducer from './features/customers/CustomerSlice';
 
-const initialState = {
-  balance: 0,
-  loan: 0,
-  loanPurpose: '',
-};
-
-function reducer(state = initialState, action) {
-  switch (action.type) {
-    case 'account/deposit':
-      return { ...state, balance: state.balance + action.payload };
-    case 'account/withdraw':
-      return { ...state, balance: state.balance - action.payload };
-    case 'account/requestLoan':
-      if (state.loan > 0) return state;
-      return {
-        ...state,
-        balance: state.balance + action.payload.amount,
-        loan: action.payload.amount,
-        loanPurpose: action.payload.purpose,
-      };
-    // LATER
-    case 'account/payLoan':
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: '',
-        balance: state.balance - state.loan,
-      };
-
-    default:
-      return state;
-  }
-}
-
-const store = createStore(reducer);
-
-store.dispatch({ type: 'account/deposit', payload: 500 });
-
-console.log(store.getState());
-
-store.dispatch({ type: 'account/withdraw', payload: 200 });
-console.log(store.getState());
-
-store.dispatch({
-  type: 'account/requestLoan',
-  payload: {
-    amount: 1000,
-    purpose: 'Buy a car',
-  },
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
 });
 
-console.log(store.getState());
+const store = createStore(rootReducer);
 
-store.dispatch({ type: 'account/payLoan' });
-console.log(store.getState());
+export default store;
